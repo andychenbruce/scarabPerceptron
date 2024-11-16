9fdbf31fe6136c2c266ad10f8ab72c230abaa96d
陈功 <chengong456@qq.com>
2024-11-16 00:59:23 +0000

    add perceptron inference, untested

--- src/bp/andy.cc
+++ src/bp/andy.cc
@@ -1,6 +1,10 @@
 /*
   by andy and neon
- */  
+ */
+#include <cassert>
+#include <cstdint>
+#include <map>
+#include <vector>
 
 #include "andy.h"
 
@@ -10,30 +14,66 @@ extern "C" {
 #include "globals/assert.h"
 #include "statistics.h"
 }
-#include <cassert>
 
-void bp_andy_init(void){
-  assert(false);
-}
-void bp_andy_timestamp(Op*){
-  assert(false);
-}
-uns8 bp_andy_pred(Op*){
-  assert(false);
-}
-void bp_andy_spec_update(Op*){
-  assert(false);
-}
-void bp_andy_update(Op*){
-  assert(false);
-}
-void bp_andy_retire(Op*){
-  assert(false);
-}
-void bp_andy_recover(Recovery_Info*){
-  assert(false);
-}
-uns8 bp_andy_full(uns){
-  assert(false);
+
+void bp_andy_timestamp(Op*) {}
+void bp_andy_spec_update(Op*) {}
+void bp_andy_retire(Op*) {}
+void bp_andy_recover(Recovery_Info*) {}
+uns8 bp_andy_full(uns) {
+  return 0;
 }
 
+class AndyPerceptron{
+private:
+  std::vector<double> weights;
+  double bias;
+public:
+  AndyPerceptron(){
+    this->weights = std::vector<double>(HIST_LENGTH, 0.0);
+    this->bias = 0;
+  }
+  bool predict(uns32 history){
+    double y = this->bias;
+
+    for(uns i = 0; i < HIST_LENGTH; i++){
+      int history_bit = (history & (1 << i)) >> i;
+
+      if(history_bit == 0){
+	y += this->weights[i] * -1;
+      }else{
+	y += this->weights[i] * 1;
+      }
+    }
+    
+    return y > 0.0;
+  }
+};
+
+std::map<Addr, AndyPerceptron> perceptron_table;
+
+void bp_andy_init(void) {}
+
+uns8 bp_andy_pred(Op* op) {
+  Addr addr = op->oracle_info.va;
+
+  if(perceptron_table.find(addr) != perceptron_table.end()){
+    AndyPerceptron* p = &perceptron_table[addr];
+    if (p->predict(op->oracle_info.pred_global_hist)){
+      return 1;
+    }else{
+      return 0;
+    }
+  }else{
+    AndyPerceptron new_p{};
+    perceptron_table[addr] = new_p;
+    if(new_p.predict(op->oracle_info.pred_global_hist)){
+      return 1;
+    }else{
+      return 0;
+    }
+  }
+}
+void bp_andy_update(Op*) {
+  assert(false);
+}
