57784f9d771af6368a410786ac9202b5cac5488f
陈功 <chengong456@qq.com>
2024-11-18 19:51:20 +0000

    add code to update perceptron only if incorrect

--- src/bp/andy.cc
+++ src/bp/andy.cc
@@ -16,6 +16,46 @@ extern "C" {
 }
 
 
+class AndyPerceptron {
+ private:
+  std::vector<double> weights;
+  double              bias;
+
+ public:
+  AndyPerceptron() {
+    this->weights = std::vector<double>(HIST_LENGTH, 0.0);
+    this->bias    = 0;
+  }
+  bool predict(uns32 history) const {
+    double y = this->bias;
+
+    for(uns i = 0; i < HIST_LENGTH; i++) {
+      int history_bit = (history & (1 << i)) >> i;
+
+      if(history_bit == 0) {
+        y += this->weights[i] * -1;
+      } else {
+        y += this->weights[i] * 1;
+      }
+    }
+
+    return y > 0.0;
+  }
+};
+
+namespace {
+struct AndyState {
+  std::vector<AndyPerceptron> pht;
+};
+uns32 get_pht_index(const Addr addr, const uns32 hist) {
+  const uns32 cooked_hist = hist >> (32 - HIST_LENGTH);
+  const uns32 cooked_addr = (addr >> 2) & N_BIT_MASK(HIST_LENGTH);
+  return cooked_hist ^ cooked_addr;
+}
+}  // namespace
+
+std::vector<AndyState> andy_state_all_cores;
+
 void bp_andy_timestamp(Op*) {}
 void bp_andy_spec_update(Op*) {}
 void bp_andy_retire(Op*) {}
@@ -24,56 +64,44 @@ uns8 bp_andy_full(uns) {
   return 0;
 }
 
-class AndyPerceptron{
-private:
-  std::vector<double> weights;
-  double bias;
-public:
-  AndyPerceptron(){
-    this->weights = std::vector<double>(HIST_LENGTH, 0.0);
-    this->bias = 0;
-  }
-  bool predict(uns32 history){
-    double y = this->bias;
-
-    for(uns i = 0; i < HIST_LENGTH; i++){
-      int history_bit = (history & (1 << i)) >> i;
-
-      if(history_bit == 0){
-	y += this->weights[i] * -1;
-      }else{
-	y += this->weights[i] * 1;
-      }
-    }
-    
-    return y > 0.0;
-  }
-};
-
-std::map<Addr, AndyPerceptron> perceptron_table;
-
-void bp_andy_init(void) {}
-
-uns8 bp_andy_pred(Op* op) {
-  Addr addr = op->oracle_info.va;
-
-  if(perceptron_table.find(addr) != perceptron_table.end()){
-    AndyPerceptron* p = &perceptron_table[addr];
-    if (p->predict(op->oracle_info.pred_global_hist)){
-      return 1;
-    }else{
-      return 0;
-    }
-  }else{
-    AndyPerceptron new_p{};
-    perceptron_table[addr] = new_p;
-    if(new_p.predict(op->oracle_info.pred_global_hist)){
-      return 1;
-    }else{
-      return 0;
-    }
+void bp_andy_init(void) {
+  andy_state_all_cores.resize(NUM_CORES);
+  for(auto& gshare_state : andy_state_all_cores) {
+    gshare_state.pht.resize(1 << HIST_LENGTH, AndyPerceptron());
   }
 }
-void bp_andy_update(Op*) {
-  assert(false);
+
+uns8 bp_andy_pred(Op* op) {
+  const uns   proc_id      = op->proc_id;
+  const auto& gshare_state = andy_state_all_cores.at(proc_id);
+
+  const Addr           addr      = op->oracle_info.pred_addr;
+  const uns32          hist      = op->oracle_info.pred_global_hist;
+  const uns32          pht_index = get_pht_index(addr, hist);
+  const AndyPerceptron pht_entry = gshare_state.pht[pht_index];
+
+  if(pht_entry.predict(hist)) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+void bp_andy_update(Op* op) {
+  if(op->table_info->cf_type != CF_CBR) {
+    return;
+  }
+
+  const uns proc_id      = op->proc_id;
+  auto&     gshare_state = andy_state_all_cores.at(proc_id);
+
+  const Addr      addr      = op->oracle_info.pred_addr;
+  const uns32     hist      = op->oracle_info.pred_global_hist;
+  const uns32     pht_index = get_pht_index(addr, hist);
+  AndyPerceptron* pht_entry = &gshare_state.pht[pht_index];
+
+
+  if(op->oracle_info.mispred) {
+    pht_entry->update(hist);
+  }
 }
